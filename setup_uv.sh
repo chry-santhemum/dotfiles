a166e4279e1e555ac073ad477666789f559ac7f0
chry-santhemum <zifanawang04@gmail.com>
2025-08-29 18:03:17 +0000

    debug

--- setup_uv.sh
+++ setup_uv.sh
@@ -2,8 +2,8 @@
 
 echo "Installing uv and Python 3.12..."
 
+cd $HOME
 curl -L https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
 uv python install 3.12
 uv venv
 source $HOME/.venv/bin/activate
