e43bf2ff47a98faf1cb88e97bd838bfa87bddaa3
chry-santhemum <zifanawang04@gmail.com>
2025-09-02 20:51:26 +0000

    add git config

--- github.sh
+++ github.sh
@@ -9,3 +9,5 @@ git config --global user.email "$email"
 git config --global user.name "$name"
 git config --global init.defaultBranch "main"
 git config --global credential.helper 'store --file=/workspace/.git-credentials'
+git config --global core.editor "cursor --wait"
+git config --global alias.lol "log --graph --oneline --decorate --color --all"
