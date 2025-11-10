5da8a0d751a380da32e2f6d8c48c12e34b6d711a
chry-santhemum <zifanawang04@gmail.com>
2025-11-10 16:18:04 +0000

    remove hf and wandb verification

--- setup_uv.sh
+++ setup_uv.sh
@@ -9,9 +9,3 @@ source $HOME/.local/bin/env
 uv python install 3.12
 uv venv
 source .venv/bin/activate
-
-echo "Configuring hf and wandb..."
-
-uv pip install "huggingface_hub" "wandb"
-hf auth login --token $RUNPOD_HF_TOKEN --add-to-git-credential
-wandb login $RUNPOD_WANDB_TOKEN
